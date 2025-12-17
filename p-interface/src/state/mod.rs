fc39a485cf1b214e16a0f13fd6ddea0cd87aaa87
ananas <jorrit@lightprotocol.com>
2025-12-17 21:50:41 +0000

    chore: allow token accounts of size 261 bytes

--- p-interface/src/state/mod.rs
+++ p-interface/src/state/mod.rs
@@ -54,7 +54,7 @@ pub unsafe fn load<T: Initializable + Transmutable>(bytes: &[u8]) -> Result<&T, 
 /// The caller must ensure that `bytes` contains a valid representation of `T`.
 #[inline(always)]
 pub unsafe fn load_unchecked<T: Transmutable>(bytes: &[u8]) -> Result<&T, ProgramError> {
-    if bytes.len() != T::LEN && bytes.len() != 260 {
+    if bytes.len() != T::LEN && bytes.len() != 261 {
         msg!("invalid account data len");
         return Err(ProgramError::InvalidAccountData);
     }
@@ -91,7 +91,7 @@ pub unsafe fn load_mut<T: Initializable + Transmutable>(
 pub unsafe fn load_mut_unchecked<T: Transmutable>(
     bytes: &mut [u8],
 ) -> Result<&mut T, ProgramError> {
-    if bytes.len() != T::LEN && bytes.len() != 260 {
+    if bytes.len() != T::LEN && bytes.len() != 261 {
         return Err(ProgramError::InvalidAccountData);
     }
     Ok(&mut *(bytes[..165].as_mut_ptr() as *mut T))
